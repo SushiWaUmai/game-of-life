64b2a72f0ba834f96c62b952a565fe658ed2709e
SushiWaUmai <eugene.bb1@gmail.com>
2022-04-20 17:26:32 +0200

    fix: :bug: remove restrict mode in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
