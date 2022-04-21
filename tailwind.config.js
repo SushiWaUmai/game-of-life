7ce75fbe31617e08d7ef09ae1464be9ac31e1d5b
SushiWaUmai <eugene.bb1@gmail.com>
2022-04-21 19:34:43 +0200

    feat: :sparkles: add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zen: ["Zen Kurenaido", "sans-serif"],
+      },
+      colors: {
+        current: "currentColor",
+        crayola: "#1E3231",
+        coral: "#485665",
+        mountbatten: "#8E7C93",
+        lavender: "#D0A5C0",
+        orchid: "#F6C0D0",
+      },
+    },
   },
   plugins: [],
-}
+};
