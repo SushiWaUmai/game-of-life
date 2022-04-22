c5d5f62031757bdff8274d8589cb352e4faf2e8c
SushiWaUmai <eugene.bb1@gmail.com>
2022-04-22 10:02:15 +0200

    feat: :sparkles: use icons for zoom button

--- src/components/SketchControls.tsx
+++ src/components/SketchControls.tsx
@@ -1,47 +1,59 @@
-import { changeZoom, clear, randomize, toDefaults, toggleLoop } from "../utils/sketch";
+import {
+  changeZoom,
+  clear,
+  randomize,
+  toDefaults,
+  toggleLoop,
+} from "../utils/sketch";
+import { BsZoomIn, BsZoomOut } from "react-icons/bs";
 
 interface SketchControlsProps {}
 
 const SketchControls: React.FC<SketchControlsProps> = () => {
   return (
-    <div className="m-4">
+    <div className="m-4 flex flex-col w-full">
       <h1 className="text-3xl">{"Conway's Game of Life"}</h1>
-      <div className="my-5 flex gap-x-5">
+      <div className="my-5 flex-grow">
+        <div className="flex gap-x-5">
+          <button
+            className="bg-stone-800 btn py-1 px-3 rounded"
+            onClick={() => clear()}
+          >
+            Clear
+          </button>
+          <button
+            className="bg-stone-800 btn py-1 px-3 rounded"
+            onClick={() => toDefaults()}
+          >
+            Reset Position
+          </button>
+          <button
+            className="bg-stone-800 btn py-1 px-3 rounded"
+            onClick={() => randomize()}
+          >
+            Randomize
+          </button>
+          <button
+            className="bg-stone-800 btn py-1 px-3 rounded"
+            onClick={() => toggleLoop()}
+          >
+            Toggle Loop
+          </button>
+        </div>
+      </div>
+
+      <div className="flex-shrink flex gap-x-3 flex-row-reverse">
         <button
           className="bg-stone-800 btn py-1 px-3 rounded"
-          onClick={() => clear()}
+          onClick={() => changeZoom(1, 0.5, 0.5)}
         >
-          Clear
-        </button>
-        <button
-          className="bg-stone-800 btn py-1 px-3 rounded"
-          onClick={() => toDefaults()}
-        >
-          Reset Position
-        </button>
-        <button
-          className="bg-stone-800 btn py-1 px-3 rounded"
-          onClick={() => randomize()}
-        >
-          Randomize
-        </button>
-        <button
-          className="bg-stone-800 btn py-1 px-3 rounded"
-          onClick={() => toggleLoop()}
-        >
-          Toggle Loop
+          <BsZoomOut />
         </button>
         <button
           className="bg-stone-800 btn py-1 px-3 rounded"
           onClick={() => changeZoom(-1, 0.5, 0.5)}
         >
-          Zoom In
-        </button>
-        <button
-          className="bg-stone-800 btn py-1 px-3 rounded"
-          onClick={() => changeZoom(1, 0.5, 0.5)}
-        >
-          Zoom Out
+          <BsZoomIn />
         </button>
       </div>
     </div>
